16c8bf19ab66e7d7da99f1b2cd98eda90299361f
KarloKac13 <karlomanojlovic@gmail.com>
2023-12-06 01:55:56 +0100

    Tweaked some styles

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,9 @@
     </div>
   </div>
   <div class="descriptionContent">
-    <p class="type-me">Hi, my name is Karlo Manojlović and welcome to my portfolio</p>
+    <p class="type-me" id="type-me">
+      <span class="cursor">|</span>
+    </p>
   </div>
 </template>
 
@@ -21,38 +23,59 @@
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
 
-
 gsap.registerPlugin(TextPlugin);
 
 export default {
+  data() {
+    return {};
+  },
+
   mounted() {
     const textElement = document.querySelector(".type-me");
-    const text = textElement.textContent.trim();
+    const text =
+      "Hello, my name is Karlo Manojlović, I'm a frontend developer and welcome to my portfolio";
     const textArray = text.split("");
 
-    textElement.textContent = ""; // Clear the original text
+    textElement.innerHTML = ""; // Clear the original text
 
     const timeline = gsap.timeline();
 
     textArray.forEach((letter, index) => {
+      // Typing animation
       timeline.to(textElement, {
         textContent: textArray.slice(0, index + 1).join(""),
-        duration: 0.1, // Adjust the duration to control the typing speed
+        duration: 0.07,
         ease: "power1.inOut",
-        onComplete: index === textArray.length - 1 ? this.onAnimationComplete : null,
+        onComplete:
+          index === textArray.length - 1 ? this.onAnimationComplete : null,
       });
     });
   },
   methods: {
     onAnimationComplete() {
-      // Any additional actions you want to perform when the typing animation is complete
+      const anchorEl = document.querySelectorAll("a");
+      const timeline = gsap.timeline();
+
+      anchorEl.forEach((anchor) => {
+        timeline.fromTo(
+          anchor,
+          {
+            duration: 0.5,
+            y: -100,
+          },
+          {
+            opacity: 1,
+            duration: 0.5,
+            y: 0,
+          }
+        );
+      });
     },
   },
 };
 </script>
 
 <style>
-
 video {
   position: fixed;
   width: 100%;
@@ -66,13 +89,14 @@ a {
   color: #001f3f;
   font-size: 25px;
   font-family: "Montserrat", sans-serif;
+  opacity: 0;
 }
 
 a:hover {
   text-shadow: 3px 3px 3px dimgray;
 }
 
-p{
+p {
   display: flex;
   flex-flow: column;
   justify-content: center;
@@ -87,13 +111,15 @@ p{
 .navBarContainer {
   display: flex;
   flex-flow: column;
-  height: 75px;
-  justify-content: end;
+  height: 10%;
+  justify-content: center;
 }
 
 .navBar {
   display: flex;
   flex-flow: row;
+  flex-wrap: wrap;
+  align-items: center;
   width: 50%;
   justify-content: space-evenly;
 }
@@ -106,7 +132,7 @@ p{
   justify-content: center;
 }
 
- .type-me {
+.type-me {
   width: 85%;
 }
 </style>
